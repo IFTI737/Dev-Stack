df82279a52bdcce7fe15d1dd29b86418402bbac4
Iftekhar Bin Shoib <hoganhulk737@gmail.com>
2026-09-13 17:18:12 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Hero from './components/Hero.tsx'
+import './components/Nav.tsx'
+import Nav from './components/Nav.tsx'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Nav/>
+    <Hero/>
+      
+    </>
+
+  )
+}
+
+export default App
